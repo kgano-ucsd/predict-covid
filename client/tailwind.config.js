4cf67b19f562bc7155ebc58541ad94f1e51d22cc
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-02-17 15:34:20 -0800

    Set up tailwind with post css - ready to test

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        olorenlight: {
+          primary: "#1D62E7",
+          secondary: "#6943D8",
+          accent: "#48DB72",
+          neutral: "#ECECEC",
+          "base-100": "#FFFFFF",
+          info: "#D5D5D5",
+          success: "#48DB72",
+          warning: "#FBBD23",
+          error: "#F94F4F",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"Rubik"', "sans-serif"],
+      body: ["Rubik", "sans-serif"],
+      display: ["Cabin", "sans-serif"],
+    },
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-}
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
 
