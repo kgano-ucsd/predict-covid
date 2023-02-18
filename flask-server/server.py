c4116d7d72c01a5c68e46fc1d5e0fc278e50feaa
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-02-18 00:54:02 -0800

    Added covid prediction repo

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,6 +2,12 @@ from flask import Flask, requests
 
 app = Flask(__name__)
 
+@app.route("/test", methods=["POST"])
+def test():
+    req_data = requests.json()
+
+    return 
+
 
 
 if __name__ == "__main__":
