591da9f9bf072487bc2f83a2fd1b47e21160a4c1
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-02-19 04:58:51 -0800

    Done with LLM for now

--- client/src/components/LLM.jsx
+++ client/src/components/LLM.jsx
@@ -28,6 +28,7 @@ Limit responses to 2 sentences, and word your explanations like I am someone who
   const handleSubmit = async (e) => {
     e.preventDefault();
     setInput("")
+    setPrompts(prompts => [...prompts, input])
     setLoading(true);
     const result = await openai.createCompletion({
       model: "text-davinci-003",
@@ -36,27 +37,22 @@ Limit responses to 2 sentences, and word your explanations like I am someone who
       max_tokens: 4000,
     });
     console.log("response", result.data.choices[0].text);
-    setPrompts(prompts => [...prompts, input])
+    
     setApiResponse(apiResponse => [...apiResponse, result.data.choices[0].text]);
     console.log("api", apiResponse)
-    setLoading(false);  
+    setLoading(false) 
 
     
   };
 
-  useEffect(() => {
-    setApiResponse(apiResponse.reverse())
-    setPrompts(prompts.reverse())
-  }, [apiResponse])
-
 
   return (
-    <div className="h-40 p-4 mt-6">
+    <div className="h-40 static p-4 mt-6">
       <div className="flex flex-col justify-center items-center  ">
-        <header>
+        <header className="sticky">
           <h1 className="text-3xl h-[20%] font-bold">Interactive Diagnosis</h1>
         </header>
-          <div className="flex flex-row justify-center items-center p-4 w-[80%]">
+          <div className="sticky flex flex-row justify-center items-center p-4 w-[80%]">
             <input type="text" 
                   placeholder="Ask to learn more..." 
                   className="input input-bordered w-[100%] text-xl font-medium h-20"
@@ -66,25 +62,25 @@ Limit responses to 2 sentences, and word your explanations like I am someone who
             /> 
             <div className="px-4">
               <button className="btn btn-primary normal-case text-white " 
-                disabled={loading || input.length === 0}
+                disabled={input.length === 0}
                 type="submit"
                 onClick={handleSubmit}>
-                  Submit
+                  {!loading ? submit : responding}
               </button>
             </div>
           </div>
-          <div className="rounded-box bg-netural py-4">
+
+          <div className="rounded-lg max-h-80% overflow-auto rounded-box bg-netural py-4">
                {apiResponse.map((res, i) => {
                 return (
-                  <div key={i}>
+                  <div>
                     <div class="chat chat-end">
                       <div class="chat-bubble chat-bubble-primary text_white">{prompts[i]}</div>
                     </div>
                     <div class="chat chat-start">
                       <div class="chat-bubble chat-bubble-neutral">{res}</div>
                     </div>
-                    
-                  </div>
+                  </div> 
                 );
                })}
                 
